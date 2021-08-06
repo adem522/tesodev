7c94e8fad9eedf8eb25393de658d8b0fa470d0b1
adem522 <adem_sahin12@hotmail.com>
2021-08-06 18:27:32 +0300

    fix update

--- database/Update.go
+++ database/Update.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -10,13 +11,12 @@ import (
 
 func Update(data bson.M, col *mongo.Collection) error {
 	data["updatedAt"] = time.Now().Add(3 * time.Hour)
-	_, err := col.UpdateOne(context.TODO(),
+	result, err := col.UpdateOne(context.TODO(),
 		bson.M{"_id": data["_id"]},
 		bson.D{{Key: "$set", Value: data}},
 	)
-
-	if err != nil {
-		return err
+	if err != nil || result.ModifiedCount == 0 {
+		return fmt.Errorf("update not completed because of empty request or %w ", err)
 	}
 	return nil
 }
