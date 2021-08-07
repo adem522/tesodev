8d74dc0161ca43f38202d4f8e3d5506c00193e5d
adem522 <adem_sahin12@hotmail.com>
2021-08-08 01:18:10 +0300

    Update Delete_test.go

--- database/tests/Delete_test.go
+++ database/tests/Delete_test.go
@@ -15,6 +15,27 @@ func TestDeleteEmptyArg(t *testing.T) {
 	}
 	err := database.Delete(nil, address.Col)
 	if err == nil {
-		t.Errorf("expected error, received err %v", err)
+		t.Errorf("expected error, received nil %v", err)
+	}
+}
+func TestDeleteWithArg(t *testing.T) {
+	client := database.Connect()
+	defer database.Close(client)
+	address := handlers.Collect{
+		Col:  client.Database("tesodev").Collection("Address"),
+		Name: "Address",
+	}
+	data := map[string]interface{}{
+		"addressLine": "example addressline",
+		"city":        "example cit",
+		"country":     "example country",
+		"cityCode":    1,
+	}
+	result2 := ""
+	result, _ := database.Create(data, address.Col, address.Col.Name())
+	result2 = result.InsertedID.(string)
+	err := database.Delete(&result2, address.Col)
+	if err != nil {
+		t.Errorf("expected nil, received err %v", err)
 	}
 }
