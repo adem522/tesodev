077ddb958ca01ffb5ce592ccfd5d24c19782b84b
adem522 <adem_sahin12@hotmail.com>
2021-08-07 23:19:03 +0300

    Update Delete_test.go

--- database/tests/Delete_test.go
+++ database/tests/Delete_test.go
@@ -1,10 +1,20 @@
 package database
 
-/*
+import (
+	"deneme-structHandler/database"
+	"deneme-structHandler/handlers"
+	"testing"
+)
+
 func TestDeleteEmptyArg(t *testing.T) {
-	err := database.Delete(nil, "Customer")
+	client := database.Connect()
+	defer database.Close(client)
+	address := handlers.Collect{
+		Col:  client.Database("tesodev").Collection("Address"),
+		Name: "Address",
+	}
+	err := database.Delete(nil, address.Col)
 	if err == nil {
 		t.Errorf("expected error, received err %v", err)
 	}
 }
-*/
