1687ac7c9339a279b8545313868e736decabbd44
adem522 <adem_sahin12@hotmail.com>
2021-08-23 19:51:28 +0300

    fix update

--- database/Update.go
+++ database/Update.go
@@ -2,17 +2,45 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func Update(data bson.M, col *mongo.Collection) bool {
-	data["updatedAt"] = time.Now().Add(3 * time.Hour)
+func Update(data map[string]interface{}, col *mongo.Collection, name string) bool {
+	data, err := updateDetermine(data, name)
+	if err != nil {
+		return false
+	}
 	result, err := col.UpdateOne(context.TODO(),
 		bson.M{"_id": data["_id"]},
 		bson.D{{Key: "$set", Value: data}},
 	)
+	fmt.Println("update error", err)
+	fmt.Println("data", data)
 	return err == nil && result.ModifiedCount != 0
+}
+
+func updateDetermine(request map[string]interface{}, name string) (bson.M, error) {
+	if name != "" {
+		if name == "Address" {
+			toInt(request, "cityCode") //if came double its return to int
+		} else if name == "Customer" || name == "Order" {
+			request["updatedAt"] = time.Now().Add(3 * time.Hour)
+			if name == "Order" {
+				toInt(request, "quantity") //if came double its return to int
+			}
+			address := request["address"].(map[string]interface{}) //address is a interface
+			toInt(address, "cityCode")                             //if came double its return to int
+		} else if name == "Product" {
+			//product just need id
+		} else {
+			return nil, fmt.Errorf(" collection name not found") //if not nil and not at all these
+		}
+	} else {
+		return nil, fmt.Errorf(" collection name must") //if name nil
+	}
+	return request, nil
 }
--- handlers/Update.go
+++ handlers/Update.go
@@ -1,19 +1,20 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 	"tesodev/database"
 
 	"github.com/labstack/echo"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (col *Collect) Update(c echo.Context) error {
-	data := bson.M{}
+	data := col.define()
 	if err := c.Bind(&data); err != nil {
+		fmt.Println("bind error", err)
 		return c.JSON(http.StatusBadRequest, false)
 	}
-	if database.Update(data, col.Col) {
+	if database.Update(data.(map[string]interface{}), col.Col, col.Name) {
 		return c.JSON(http.StatusOK, true)
 	}
 	return c.JSON(http.StatusBadRequest, false)
